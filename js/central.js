755b82ac96a645b5ab0039335338fd8be7676ca7
Emma Baker <emma.baker@wustl.edu>
2021-04-13 22:21:11 -0500

    Updated data

--- js/central.js
+++ js/central.js
@@ -1,9 +1,7 @@
 
-function CentralChart(occupancyData, permitData, demolitionData){
+function CentralChart(worldData){
     let self = this;
-    self.occupancyData = occupancyData;
-    self.permitData = permitData;
-    self.demolitionData = demolitionData;
+    self.worldData = worldData;
     self.init();
 };
 
@@ -16,84 +14,85 @@ CentralChart.prototype.init = function(){
     
 
     const svg = d3.select("#central").append("svg")
-        .attr("width", 900)
-        .attr("height", 600)
+        .attr("width", 1200)
+        .attr("height", 1000)
         .attr("class", "central");
 
-    var x = d3.scaleLinear()
-        .domain(d3.extent(self.permitData, function(d) { return +d.YEAR; }))
+    var xScale = d3.scaleLinear()
+        .domain(d3.extent(self.worldData, function(d) { return +d.Year; }))
         .range([ 0, 800 ]);
+
     svg.append("g")
-        .attr("transform", "translate(50," + 500 + ")")
-        .call(d3.axisBottom(x)
-            .ticks(5)
+        .attr("transform", "translate(400," + 500 + ")")
+        .call(d3.axisBottom(xScale)
+            .ticks(21)
             .tickFormat(d3.format(".0f"))
         );
 
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(self.permitData, function(d) { return +d.TOTALNUMBER; })])
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(self.worldData, function(d) { 
+            return +d.World; 
+        })])
         .range([ 450, 0 ]);
     svg.append("g")
-        .attr("transform", "translate(50," + 50 + ")")
-        .call(d3.axisLeft(y));
+        .attr("transform", "translate(400," + 50 + ")")
+        .call(d3.axisLeft(yScale));
 
-    svg.append("path")
-      .datum(self.occupancyData)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 3)
-      .attr("d", d3.line()
-        .x(function(d) { 
-            return x(+d.YEAR) 
-        })
-        .y(function(d) { 
-            return y(+d.TOTALNUMBER) 
-        }))
-        .attr("transform", "translate(50," + 50 + ")")
+
+    var lines = svg.selectAll("line").data(self.worldData);
+    lines.enter()
+      .append("path")
+        .attr("stroke", "black")
+        .attr("stroke-width", 3)
+        .attr("d", d3.line()
+            .x(function(d) { return x(+d.Year); })
+            .y(function(d) { return y(+d.World); }))
+        .attr("transform", "translate(400," + 50 + ")")
+    lines.exit().remove()
         
 
-    svg.append("path")
-      .datum(self.permitData)
-      .attr("fill", "none")
-      .attr("stroke", "red")
-      .attr("stroke-width", 3)
-      .attr("d", d3.line()
-        .x(function(d) { 
-            return x(+d.YEAR) 
-        })
-        .y(function(d) { 
-            return y(+d.TOTALNUMBER) 
-        }))
-        .attr("transform", "translate(50," + 50 + ")")
+    // svg.append("path")
+    //   .datum(self.permitData)
+    //   .attr("fill", "none")
+    //   .attr("stroke", "red")
+    //   .attr("stroke-width", 3)
+    //   .attr("d", d3.line()
+    //     .x(function(d) { 
+    //         return x(+d.YEAR) 
+    //     })
+    //     .y(function(d) { 
+    //         return y(+d) 
+    //     }))
+    //     .attr("transform", "translate(500," + 50 + ")")
 
-    svg.append("path")
-      .datum(self.demolitionData)
-      .attr("fill", "none")
-      .attr("stroke", "green")
-      .attr("stroke-width", 3)
-      .attr("d", d3.line()
-        .x(function(d) { 
-            return x(+d.YEAR) 
-        })
-        .y(function(d) { 
-            return y(+d.TOTALNUMBER) 
-        }))
-        .attr("transform", "translate(50," + 50 + ")")
+    // svg.append("path")
+    //   .datum(self.demolitionData)
+    //   .attr("fill", "none")
+    //   .attr("stroke", "green")
+    //   .attr("stroke-width", 3)
+    //   .attr("d", d3.line()
+    //     .x(function(d) { 
+    //         return x(+d.YEAR) 
+    //     })
+    //     .y(function(d) { 
+    //         return y(+d.TOTALNUMBER) 
+    //     }))
+    //     .attr("transform", "translate(500," + 50 + ")")
 
     svg.append("text")
         .attr("y", 550)
-        .attr("x", 425)
+        .attr("x", 850)
         .attr("class", "labels")
         .text("Year");
     
     svg.append("text")
         .attr("class", "labels")
         .attr("text-anchor", "end")
-        .attr("y", 0)
+        .attr("y", 300)
         .attr("x", -200)
         .attr("dy", ".75em")
         .attr("transform", "rotate(-90)")
-        .text("Number of permits");
+        .text("Number of people");
 
     
 }
--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@
   var instance = null;
   function init() {
     Promise.all([
-      d3.csv("data/occupancy.csv"),
-      d3.csv("data/permits.csv"),
-      d3.csv("data/demolition.csv"),
+      d3.csv("data/world.csv"),
     ]).then(function (files) {
         var centralChart = new CentralChart(files[0], files[1], files[2]); 
-        var occupancy = new OccupancyChart(files[0]);
-        var permits = new PermitChart(files[1]);
-        var demolition = new DemolitionChart(files[2]);
+        // var occupancy = new OccupancyChart(files[0]);
+        // var permits = new PermitChart(files[1]);
+        // var demolition = new DemolitionChart(files[2]);
     });
   }
 
