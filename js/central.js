9e9aabd6803af51dd3227420c8477d0ed5ec12e7
emlorraine <52936908+emlorraine@users.noreply.github.com>
2021-04-20 10:10:00 -0500

    Update central.js

--- js/central.js
+++ js/central.js
@@ -22,7 +22,7 @@ CentralChart.prototype.init = function(){
 
     //Scale
     var xScale = d3.scaleLinear()
-        .domain([1999, 2019])
+        .domain([1960, 2019])
         .range([ padding, self.svgWidth - padding ]);
 
     var yScale = d3.scaleLinear()
